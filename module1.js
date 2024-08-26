ed79e314f75ac111d0f1595eebfd2347063c4b0a
foryomacaroll <foryoma.caroll@gmail.com>
2024-08-26 23:05:03 +0700

    ep4-final

---
+++ module1.js
@@ -0,0 +1,5 @@
+const myname = "kyaw zayar";
+const myage = 23;
+const another = "one";
+
+module.exports = { myname, myage, another };
---
+++ module2.js
@@ -0,0 +1,3 @@
+const { myname, myage, another } = require("./module1.js");
+
+console.log(myname, another);
