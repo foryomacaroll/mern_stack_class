5b646ab6a2a5f2517d744c02490b2f642ff0e252
foryomacaroll <foryoma.caroll@gmail.com>
2024-08-27 02:40:25 +0700

    ep9_final_Route_Page_NodeJS_basic

--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
+  //   console.log(req.url);
+  let filename;
+  switch (req.url) {
+    case "/":
+      filename = "home.html";
+      break;
+    case "/contact":
+      filename = "contact.html";
+      break;
+    case "/about":
+      filename = "about.html";
+      break;
+    default:
+      filename = "404.html";
+      break;
+  }
+
   res.setHeader("Content-Type", "text/html");
   // inspect/ network/ all/ localhost/ header/ response header/ content-type
-  fs.readFile("./views/home.html", (err, data) => {
+  fs.readFile("./views/" + filename, (err, data) => {
     if (err) {
       console.log(err);
       res.end();
