0e3dca937886010504f2fb6f056908206578f660
foryomacaroll <foryoma.caroll@gmail.com>
2024-08-27 16:13:52 +0700

    ep15-final_redirect-pages_404-page-handling_(app.use() Method)

--- app.js
+++ app.js
@@ -10,9 +10,24 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.sendFile("./views/about.html", { root: __dirname });
 });
+app.get("/about-us", (req, res) => {
+  // Redirect to /about page
+  res.redirect("/about");
+});
 app.get("/contact", (req, res) => {
   res.sendFile("./views/contact.html", { root: __dirname });
 });
+app.use((req, res) => {
+  // Fall-Back
+  res.status(404);
+  res.sendFile("./views/404.html", { root: __dirname });
+});
+
+// app.use must be write in end of route codes.
+// Because, if a routes comes in, express js will search the route from up to down
+// if the route match the method, it will not search for remaining codes.
+/* *** if we write app.use between routes, the method routes under app.use will never
+be found anymore. */
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
